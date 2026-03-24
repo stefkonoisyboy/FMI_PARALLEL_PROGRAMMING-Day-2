ca17bbaee0621783fd446c547d71b5418e3feb8f
PC <stefko_coniovski@abv.bg>
2026-03-24 22:59:39 +0200

    Solved P02

---
+++ P02/Program.cs
@@ -0,0 +1,346 @@
+﻿using System.Diagnostics;
+
+namespace P02;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string text =
+            @"В старо време нямало прилепи. Имало — между многото видове мишки — една с широки, заострени уши и с триъгълни уста. Тя била много хитра и завистлива. Като гледала крилатото куче как лети по всички посоки, много й се искало да има крила като неговите. Тогава нямало куче без крила. Мишката постоянно мислела как да се снабди с крила, колкото и малки да са те.
+
+Тя си казвала често, като гледала как кучето се издига високо-високо над къщите и над дърветата:
+
+— Няма правда на света. Аз съм от голямо семейство, от знатен род. А кучето от какъв род е? От никакъв. Никой му не знае дедите и прадедите. Пък ето че то хвърчи ли — хвърчи, а аз — аз съм принудена да се тътря по̀земи[1].
+
+За да се снабди с крила, мишката трябвало да направи нещо. И ето що направила.
+
+Излязла и отишла в гората. Покатерила се на едно дърво, което раждало вкусни червени плодове. Тя знаела, че кучето много ги обича. Дори и в самия този миг то било на дървото и ядяло от плодовете му. Кучето видяло мишката и се уплашило. А тя му се поклонила и го поздравила любезно.
+
+— Добър ден, чужденецо — рекла. — Кога си дошъл по нашите места?
+
+Преди да изслуша тоя поздрав, крилатото куче хвръкнало и кацнало на друго дърво, пак с такива плодове. Мишката слязла. Тя се покатерила на другото дърво. Щом я видяло, кучето понечило да литне, но тя го спряла с любезно движение и му казала:
+
+— Стой, чужденецо! Моля те, недей се плаши от мене! Много ми е драго, че ти се нравят плодовете на дървото ми. Готова съм да ти дам колкото искаш от тях — и то безплатно. Постой да си поговорим малко!
+
+— Не зная за какво можем да говорим ние с тебе — рекло плахо кучето. — Аз не те познавам.
+
+— Личи си — казала мишката. — Ако ме познаваш, щеше да знаеш, че дървото, на което седиш, е мое.
+
+— Тъй ли? — рекло зачудено кучето. — Пръв път чувам, че това дърво е нечие. Аз мислех, че то няма господар.
+
+— Там ти е грешката. То е мое и ти не бива да ядеш от плодовете му, без мое позволение.
+
+— Извини ме, господарке! Друг път няма и да кацам на него. Ще намеря друго дърво.
+
+— Кое? Всички дървета тук са мои. Моя е цялата гора, от край до край.
+
+Кучето се уплашило и отново се приготвило да хвръкне. Мишката го спряла пак.
+
+— Почакай! Не сме си свършили разговора.
+
+— Че какво има още да приказваме? Говори по-скоро: имам работа, а вкъщи ме чакат децата.
+
+— Там е работата, че да беше друг на мое място — щеше без друго да те глоби. Законът не позволява да се ядат чужди плодове. То се казва кражба.
+
+Кучето се уплашило.
+
+— Не разбирам, що искаш да кажеш. Пръв път чувам такива думи.
+
+— Не бой се: аз съм добра и няма да те глобя. Стой си на дървото. И идвай, когато поискаш! Яж от моите плодове: аз ти не браня[2]!
+
+— Благодаря ти. Ти си добра господарка.
+
+В това време мишката се натъкмила до крилатото куче и почнала да го разглежда отблизо. После го запитала:
+
+— Не те ли е страх да летиш по небето?
+
+— Не — отвърнало кучето. — Защо ще ме е страх?
+
+— Че нали е високо? Ами ако паднеш? Мене ми се завива свяст, само като видя някоя птица, че лети.
+
+Кучето казало с усмивка:
+
+— То е — защото никога не си летяла. Горе не е никак страшно. Па и аз не хвърча много на високо. И — най-сетне — как ще падна, като имам крила?
+
+— Тъй ли? Дай да ти видя крилата.
+
+Поразгледала ги мишката, па рекла:
+
+— Хубави и здрави изглеждат. Но не ми се вярва да може да се лети дълго с тях. Те са от кожа, нямат пера. А тия на птиците са с пера. Те са истински крила.
+
+Кучето се докачило малко.
+
+— Та какво, че моите крила били без пера? Те не са по-лоши от птичите. И с тях се хвърчи добре.
+
+— Тъкмо това не мога да повярвам. Но то може да се провери. Дай ми ги да ги опитам.
+
+— Да ти ги дам. Но разбира се, само за малко, защото аз бързам.
+
+И крилатото куче откачило крилата от гърба си, натъкмило ги на мишката и й разправило как да хвърчи.
+
+— Ще видиш, че е много лесна работа. Но недей летя надалеч, че не мога дълго да те чакам.
+
+— Бъди спокоен! Докле[3] ме чакаш, наяж се с плодове от моето дърво. А аз ще отлетя ей до онова дърво и ще се върна веднага.
+
+И мишката хвръкнала. Кучето почнало да яде. То изяло много плодове — много повече от друг път. Преяло, очаквайки мишката да се върне и да му предаде обратно крилата. Но тя все не се връщала. И нямала намерение да се върне.
+
+Дълго се озъртало кучето — дано я види. Но никъде не я видяло. Тя отлетяла много надалече.
+
+Кучето почнало да я вика:
+
+— Господарке, мила господарке! Върни се! Дай ми крилата! Върни се, моля ти се! Върни се!
+
+А мишката се не връщала. Тя летяла все по-далече и по-далече.
+
+Тогава кучето се разплакало.
+
+— Едно малко животно ми открадна крилата. Помогнете ми! Едно малко животно отлетя с моите крила.
+
+Но никой не чул плача на кучето. Никой не дошъл да му помогне. Тогава то скочило от дървото и тръгнало да търси мишката.
+
+— „Бау-бау-бау! — Джаф-джаф джаф!“ — викнало то, с което искало да каже! „Крилата-крилата-крилата! — Дайте ги! Дайте ги! Дайте ги!“
+
+Оттогава кучето живее на земята, върви и лае: търси си крилата. А прилепът — мишката с крилата — лети по въздуха, но само нощем, защото се бои, че кучето ще я намери и ще й вземе крилата. Затова прилепът не смее да каца около къщите, дори и през нощта — да не би кучето да го подуши.";
+
+        Benchmark(() => ProcessLinear(text), "Linear");
+        Benchmark(() => ProcessParallel(text), "Parallel");
+    }
+
+    static void Benchmark(Action action, string name)
+    {
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
+        action();
+        stopwatch.Stop();
+        Console.WriteLine($"{name}: {stopwatch.ElapsedMilliseconds} ms.");
+    }
+
+    static void ProcessLinear(string text)
+    {
+        FindAllWordsCount(text);
+        FindShortestWord(text);
+        FindLongestWord(text);
+        FindAverageWordLength(text);
+        Find5MostRepeatedWords(text);
+        Find5LeastRepeatedWords(text);
+    }
+
+    static void ProcessParallel(string text)
+    {
+        Thread[] threads = new Thread[6];
+        
+        threads[0] = new Thread(() => FindAllWordsCount(text));
+        threads[0].Start();
+        
+        threads[1] = new Thread(() => FindShortestWord(text));
+        threads[1].Start();
+        
+        threads[2] = new Thread(() => FindLongestWord(text));
+        threads[2].Start();
+        
+        threads[3] = new Thread(() => FindAverageWordLength(text));
+        threads[3].Start();
+        
+        threads[4] = new Thread(() => Find5MostRepeatedWords(text));
+        threads[4].Start();
+        
+        threads[5] = new Thread(() => Find5LeastRepeatedWords(text));
+        threads[5].Start();
+
+        for (int i = 0; i < threads.Length; i++)
+        {
+            threads[i].Join();
+        }
+    }
+
+    static void FindAllWordsCount(string text)
+    {
+        int count = 0;
+        string currentWord = string.Empty;
+
+        for (int i = 0; i < text.Length; i++)
+        {
+            if (char.IsLetter(text[i]))
+            {
+                currentWord += text[i];
+            }
+            else
+            {
+                if (currentWord.Length >= 3)
+                {
+                    count++;
+                }
+
+                currentWord = string.Empty;
+            }
+        }
+
+        Console.WriteLine($"Words count: {count}");
+    }
+
+    static void FindShortestWord(string text)
+    {
+        string shortestWord = string.Empty;
+        string currentWord = string.Empty;
+
+        for (int i = 0; i < text.Length; i++)
+        {
+            if (char.IsLetter(text[i]))
+            {
+                currentWord += text[i];
+            }
+            else
+            {
+                if (currentWord.Length >= 3 && (string.IsNullOrEmpty(shortestWord) || currentWord.Length < shortestWord.Length))
+                {
+                    shortestWord = currentWord;
+                }
+
+                currentWord = string.Empty;
+            }
+        }
+
+        Console.WriteLine($"Shortest word: {shortestWord}");
+    }
+    
+    static void FindLongestWord(string text)
+    {
+        string longestWord = string.Empty;
+        string currentWord = string.Empty;
+
+        for (int i = 0; i < text.Length; i++)
+        {
+            if (char.IsLetter(text[i]))
+            {
+                currentWord += text[i];
+            }
+            else
+            {
+                if (currentWord.Length >= 3 && (string.IsNullOrEmpty(longestWord) || currentWord.Length > longestWord.Length))
+                {
+                    longestWord = currentWord;
+                }
+
+                currentWord = string.Empty;
+            }
+        }
+
+        Console.WriteLine($"Shortest word: {longestWord}");
+    }
+    
+    static void FindAverageWordLength(string text)
+    {
+        int count = 0;
+        double sum = 0;
+        string currentWord = string.Empty;
+
+        for (int i = 0; i < text.Length; i++)
+        {
+            if (char.IsLetter(text[i]))
+            {
+                currentWord += text[i];
+            }
+            else
+            {
+                if (currentWord.Length >= 3)
+                {
+                    count++;
+                    sum += currentWord.Length;
+                }
+
+                currentWord = string.Empty;
+            }
+        }
+
+        if (count > 0)
+        {
+            double averageLength = sum / count;
+            string formattedAverageLength = averageLength.ToString("f2");
+
+            Console.WriteLine($"Words count: {formattedAverageLength}");
+        }
+    }
+
+    static void Find5MostRepeatedWords(string text)
+    {
+        Dictionary<string, int> words = new Dictionary<string, int>();
+        string currentWord = string.Empty;
+
+        for (int i = 0; i < text.Length; i++)
+        {
+            if (char.IsLetter(text[i]))
+            {
+                currentWord += text[i];
+            }
+            else
+            {
+                if (currentWord.Length >= 3)
+                {
+                    if (words.ContainsKey(currentWord))
+                    {
+                        words[currentWord]++;
+                    }
+                    else
+                    {
+                        words.Add(currentWord, 1);
+                    }
+                }
+
+                currentWord = string.Empty;
+            }
+        }
+
+        Dictionary<string, int> top5Words = words
+            .OrderByDescending(w => w.Value)
+            .ThenBy(w => w.Key)
+            .Take(5)
+            .ToDictionary();
+        
+        foreach (var pair in top5Words)
+        {
+            Console.WriteLine($"{pair.Key}: {pair.Value} times");
+        }
+    }
+
+    static void Find5LeastRepeatedWords(string text)
+    {
+        Dictionary<string, int> words = new Dictionary<string, int>();
+        string currentWord = string.Empty;
+
+        for (int i = 0; i < text.Length; i++)
+        {
+            if (char.IsLetter(text[i]))
+            {
+                currentWord += text[i];
+            }
+            else
+            {
+                if (currentWord.Length >= 3)
+                {
+                    if (words.ContainsKey(currentWord))
+                    {
+                        words[currentWord]++;
+                    }
+                    else
+                    {
+                        words.Add(currentWord, 1);
+                    }
+                }
+
+                currentWord = string.Empty;
+            }
+        }
+
+        Dictionary<string, int> top5Words = words
+            .OrderBy(w => w.Value)
+            .ThenBy(w => w.Key)
+            .Take(5)
+            .ToDictionary();
+        
+        foreach (var pair in top5Words)
+        {
+            Console.WriteLine($"{pair.Key}: {pair.Value} times");
+        }
+    }
+}
